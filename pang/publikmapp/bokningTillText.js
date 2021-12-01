354cc445243baa8ee239bf18fc94b45c3d94102c
Profetzorgen <matti.heinonen1988@gmail.com>
2021-12-01 14:42:02 +0100

    Första försöket

---
+++ pang/publikmapp/bokningTillText.js
@@ -0,0 +1,37 @@
+let saveFile = () => {
+    	
+    // Get the data from each element on the form.
+    const datum = document.getElementById('txtDate');
+    const antal = document.getElementById('txtAntal');
+    const tid = document.getElementById('txtTid');
+    const name = document.getElementById('txtName');
+    const email = document.getElementById('txtEmail');
+    const msg = document.getElementById('msg');
+    
+    // This variable stores all the data.
+    let data = 
+        '\r Datum: ' + datum.value + ' \r\n ' +
+        'Antal: ' + antal.value + ' \r\n ' + 
+        'Tid: ' + tid.value + ' \r\n ' + 
+        'Email: ' + email.value + ' \r\n ' + 
+        'Name:  '+ name.value +  ' \r\n ' + 
+        'Message: ' + msg.value;
+    
+    // Convert the text to BLOB.
+    const textToBLOB = new Blob([data], { type: 'text/plain' });
+    const sFileName = 'formData.txt';	   // The file to save the data.
+
+    let newLink = document.createElement("a");
+    newLink.download = sFileName;
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    }
+    else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click(); 
+}
